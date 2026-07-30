ca7882d445e09c1cf31b20638b9c8940d63f8ba3
Thomas <thomas3.colombo@mail.polimi.it>
2026-07-30 16:55:13 +0200

    successfully synched with the esp32

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(3000); // Attesa per dare tempo alla porta USB di inizializzarsi
+
+  Serial.println("\n--- Test Memoria ESP32-S3 ---");
+  
+  // Ottiene la dimensione della PSRAM in byte
+  uint32_t psram_size = ESP.getPsramSize();
+  
+  if (psram_size > 0) {
+    Serial.printf("PSRAM Trovata: %d bytes (circa %d MB)\n", psram_size, psram_size / (1024 * 1024));
+    Serial.printf("PSRAM Libera: %d bytes\n", ESP.getFreePsram());
+  } else {
+    Serial.println("Nessuna PSRAM rilevata su questa scheda.");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("Esecuzione del loop principale...");
+  delay(1000);
 }
